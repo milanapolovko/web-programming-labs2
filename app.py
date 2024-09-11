baa656dbe2c909632a8a698622619b16f309d715
Половко Милана <milana.polovko03@gmail.com>
2024-09-11 17:41:44 +0700

    Информация об авторе

--- app.py
+++ app.py
@@ -2,10 +2,28 @@ from flask import Flask
 app=Flask(__name__)
 
 @app.route("/")
-def start ():
+@app.route("/web")
+def web():
     return """<!doctype html>
         <html> 
             <body>
                 <h1>web-сервер на flask</h1>
-                    </body>
-                        </html>"""
+                <a href="/web">author</a>
+            </body>
+        </html>"""
+
+@app.route("/author")
+def author():
+    name="Половко Милана Андреевна"
+    group="ФБИ-21"
+    faculty="ФБ"
+
+    return """<!doctype html>
+        <html>
+            <body>
+                <p> Студент: """ + name + """</p>
+                <p> Группа: """ + group + """</p>
+                <p> Факультет: """ + faculty + """</p>
+                <a href="/web">web</a>
+            </body>
+        </html>"""
