44b31188badd50753dcc864f27190e9cd46e622d
Половко Милана <milana.polovko03@gmail.com>
2024-09-18 12:40:30 +0700

    Исправление адресов

--- app.py
+++ app.py
@@ -2,20 +2,20 @@ from flask import Flask, url_for, redirect
 app=Flask(__name__)
 
 @app.route("/")
-@app.route("/web")
+@app.route("/lab1/web")
 def web():
     return """<!doctype html>
         <html> 
             <body>
                 <h1>web-сервер на flask</h1>
-                <a href="/author">author</a>
+                <a href="/lab1/author">author</a>
             </body>
         </html>""", 200, {
             'X-Server':'sample',
             'Content-Type': 'text/html; charset=utf-8'
         }
 
-@app.route("/author")
+@app.route("/lab1/author")
 def author():
     name="Половко Милана Андреевна"
     group="ФБИ-21"
@@ -27,7 +27,7 @@ def author():
                 <p> Студент: """ + name + """</p>
                 <p> Группа: """ + group + """</p>
                 <p> Факультет: """ + faculty + """</p>
-                <a href="/web">web</a>
+                <a href="/lab1/web">web</a>
             </body>
         </html>"""
 
@@ -64,9 +64,9 @@ def conter():
 </html>
 '''
 
-@app.route("/info")
+@app.route("/lab1/info")
 def info():
-    return redirect("/author")
+    return redirect("/lab1/author")
 
 @app.route('/lab1/created')
 def created():
