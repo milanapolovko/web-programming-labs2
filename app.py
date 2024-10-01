048103a3fed240b25385402a640c5f30f5eaf578
Половко Милана <milana.polovko03@gmail.com>
2024-10-01 20:24:18 +0700

    Имя студента

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, redirect
+from flask import Flask, url_for, redirect, render_template
 app=Flask(__name__)
 
 @app.route("/")
@@ -371,4 +371,7 @@ def add_flower(name):
         </body>
      </html>   
     ''' 
-   
+@app.route('/lab2/example')
+def example():
+    name='Половко Милана'
+    return render_template('example.html', name=name)  
