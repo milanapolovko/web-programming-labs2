10c419629cafabe9f6b2f13a15134f4bd6a2dba8
Половко Милана <milana.polovko03@gmail.com>
2024-10-30 12:50:37 +0700

    Арифметика

--- lab4.py
+++ lab4.py
@@ -9,6 +9,22 @@ def lab():
 def div_form():
     return render_template('lab4/div-form.html')
 
+@lab4.route('/lab4/sum-form')
+def sum_form():
+    return render_template('lab4/sum-form.html')
+
+@lab4.route('/lab4/mult-form')
+def mult_form():
+    return render_template('lab4/mult-form.html')
+
+@lab4.route('/lab4/sub-form')
+def sub_form():
+    return render_template('lab4/sub-form.html')
+
+@lab4.route('/lab4/degree-form')
+def degree_form():
+    return render_template('lab4/degree-form.html')
+
 @lab4.route('/lab4/div', methods=['POST'])
 def div():
     x1=request.form.get('x1')
@@ -21,4 +37,55 @@ def div():
     x2=int(x2)
     result=x1/x2
     return render_template('lab4/div.html', x1=x1,x2=x2,result=result)
+
+@lab4.route('/lab4/sum', methods=['POST'])
+def sum():
+    x1=request.form.get('x1')
+    x2=request.form.get('x2')
+    if x1=='':
+        x1=0
+    if x2=='': 
+        x2=0   
+    x1=int(x1)
+    x2=int(x2)
+    result=x1+x2
+    return render_template('lab4/sum.html', x1=x1,x2=x2,result=result)
+
+@lab4.route('/lab4/mult', methods=['POST'])
+def mult():
+    x1=request.form.get('x1')
+    x2=request.form.get('x2')
+    if x1=='':
+        x1='1'
+    if x2=='': 
+        x2='1'   
+    x1=int(x1)
+    x2=int(x2)
+    result=x1*x2
+    return render_template('lab4/mult.html', x1=x1,x2=x2,result=result)
+
+@lab4.route('/lab4/sub', methods=['POST'])
+def sub():
+    x1=request.form.get('x1')
+    x2=request.form.get('x2')
+    if x1=='' or x2=='':
+        return render_template('lab4/sub.html',error='Оба поля должны быть заполнены!')
+    x1=int(x1)
+    x2=int(x2)
+    result=x1-x2
+    return render_template('lab4/sub.html', x1=x1,x2=x2,result=result)
+
+@lab4.route('/lab4/degree', methods=['POST'])
+def degree():
+    x1=request.form.get('x1')
+    x2=request.form.get('x2')
+    if x1=='' or x2=='':
+        return render_template('lab4/degree.html',error='Оба поля должны быть заполнены!')
+    if x2=='0' or x1=='0':
+        return render_template('lab4/degree.html',error='Поле не должно равняться 0!')
+    x1=int(x1)
+    x2=int(x2)
+    result=x1**x2
+    return render_template('lab4/degree.html', x1=x1,x2=x2,result=result)
+
 
