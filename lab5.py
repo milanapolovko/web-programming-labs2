2aeba340725c66d308662961eb45926c8837f705
Половко Милана <milana.polovko03@gmail.com>
2024-11-13 13:26:46 +0700

    Регистрация

--- lab5.py
+++ lab5.py
@@ -1,6 +1,35 @@
 from flask import Blueprint, redirect, render_template, request, session
+import psycopg2
 lab5=Blueprint('lab5',__name__)
 
 @lab5.route('/lab5/')
 def lab():
     return render_template('lab5/lab5.html')
+
+@lab5.route('/lab5/register', methods=['GET','POST'])
+def register():
+    if request.method=='GET':
+        return render_template('lab5/register.html')
+    login=request.form.get('login')
+    password=request.form.get('password')
+    if not (login and password):
+        return render_template('lab5/register.html',error='Заполните все поля', login=login)
+
+    conn=psycopg2.connect(
+        host='127.0.0.1',
+        database='milana_polovko_knowledge_base',
+        user='milana_polovko_knowledge_base',
+        password='rfifvfkfif44'
+    )
+    cur=conn.cursor()
+
+    cur.execute(f"SELECT login FROM users WHERE login='{login}';")
+    if cur.fetchone():
+        cur.close()
+        conn.close()
+        return render_template('lab5/register.html',error="Такой пользователь уже существует")
+    cur.execute(f"INSERT INTO users (login,password) VALUES ('{login}','{password}');")
+    conn.commit()
+    cur.close()
+    conn.close()
+    return render_template('lab5/success.html',login=login)
