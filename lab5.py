5cbfd34aa5e22fae1df69d70b289216dc1dc4133
Половко Милана <milana.polovko03@gmail.com>
2024-11-13 13:50:13 +0700

    Авторизация

--- lab5.py
+++ lab5.py
@@ -1,10 +1,11 @@
 from flask import Blueprint, redirect, render_template, request, session
 import psycopg2
+from psycopg2.extras import RealDictCursor
 lab5=Blueprint('lab5',__name__)
 
 @lab5.route('/lab5/')
 def lab():
-    return render_template('lab5/lab5.html')
+    return render_template('lab5/lab5.html',login=session.get('login'))
 
 @lab5.route('/lab5/register', methods=['GET','POST'])
 def register():
@@ -21,6 +22,7 @@ def register():
         user='milana_polovko_knowledge_base',
         password='rfifvfkfif44'
     )
+
     cur=conn.cursor()
 
     cur.execute(f"SELECT login FROM users WHERE login='{login}';")
@@ -33,3 +35,38 @@ def register():
     cur.close()
     conn.close()
     return render_template('lab5/success.html',login=login)
+
+@lab5.route('/lab5/login', methods=['GET','POST'])
+def login():
+    if request.method=='GET':
+        return render_template('lab5/login.html')
+    login=request.form.get('login')
+    password=request.form.get('password')
+    if not (login and password):
+        return render_template('lab5/login.html',error='Заполните все поля', login=login)
+    
+    conn=psycopg2.connect(
+        host='127.0.0.1',
+        database='milana_polovko_knowledge_base',
+        user='milana_polovko_knowledge_base',
+        password='rfifvfkfif44'
+    )
+
+    cur=conn.cursor(cursor_factory=RealDictCursor)
+
+    cur.execute(f"SELECT * FROM users WHERE login='{login}';")
+    user=cur.fetchone()
+    if not user:
+        cur.close()
+        conn.close()
+        return render_template('lab5/login.html', error='Логин и/или пароль неверны')
+    if user['password']!=password:
+        cur.close()
+        conn.close()
+        return render_template('lab5/login.html',error='Логин и/или пароль невырны')
+    
+    session['login']=login
+    cur.close()
+    conn.close()
+    return render_template('lab5/success_login.html',login=login)
+
