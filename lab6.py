5a4a21e11d020ec916709d658876b8eb11add54d
Половко Милана <milana.polovko03@gmail.com>
2024-11-27 13:55:22 +0700

    Снятие аренды

--- lab6.py
+++ lab6.py
@@ -36,7 +36,8 @@ def api():
             },
             'id': id   
         }
-    elif data ['method'] == 'booking':
+    
+    if data ['method'] == 'booking':
         office_number = data['params']
         for office in offices:
             if office['number']==office_number:
@@ -56,6 +57,37 @@ def api():
                     'result':'success',
                     'id': id
                 }
+    
+    if data['method']=='cancellation':
+        office_number=data['params']
+        for office in offices:
+            if office['number']==office_number:
+                if office['tenant'] =='':
+                    return{
+                        'jsonrpc': '2.0',
+                        'error':{
+                            'code': 3,
+                            'message': 'not booked'
+                        },
+                        'id': id
+                    }
+                if office['tenant'] != login:
+                    return{
+                        'jsonrpc': '2.0',
+                        'error':{
+                            'code': 4,
+                            'message': 'not booked'
+                        },
+                        'id': id
+                    }
+            
+                office['tenant'] =''
+                return{
+                    'jsonrpc': '2.0',
+                    'result':'success',
+                    'id': id
+                }
+
     return{
         'jsonrpc': '2.0',
         'error':{
