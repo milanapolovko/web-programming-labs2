c6fed57f8bcc4767fca1cc34566065f0e25eaacf
Половко Милана <milana.polovko03@gmail.com>
2024-11-27 11:35:52 +0700

    Заготовка к 6-ой лабораторной

---
+++ lab6.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, redirect, render_template, request, session, current_app
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from werkzeug.security import check_password_hash, generate_password_hash
+import sqlite3
+from os import path
+
+lab6=Blueprint('lab6',__name__)
+
+@lab6.route('/lab6/')
+def main():
+    return render_template('lab6/lab6.html')
