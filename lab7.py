c87fa59471a233fe2df48e8a82a749006e9e5cba
Половко Милана <milana.polovko03@gmail.com>
2024-12-06 13:15:40 +0700

    Добавление нового фильма по REST

--- lab7.py
+++ lab7.py
@@ -100,3 +100,11 @@ def put_film(id):
             },}
     films[id]=film
     return films[id]
+
+
+@lab7.route('/lab7/rest-api/films/', methods=['POST'])
+def add_film(): 
+    film=request.get_json()
+    films.append(film)
+    return len(films) - 1
+        
