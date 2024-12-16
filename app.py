088cc9df10155e5566d49a1bceccd910f0348c83
Половко Милана <milana.polovko03@gmail.com>
2024-12-16 20:10:29 +0700

    Заготовка к 9-ой лабораторной

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
 from lab8 import lab8
+from lab9 import lab9
 
 app=Flask(__name__)
 
@@ -33,6 +34,8 @@ app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
 app.register_blueprint(lab8)
+app.register_blueprint(lab9)
+
 
 
 app.config['SECRET_KEY']= os.environ.get('SECRET_KEY','секретно-секретный секрет')
@@ -74,6 +77,7 @@ def title():
                     <li><a href="/lab6/">Шестая лабораторная работа</a></li>
                     <li><a href="/lab7/">Седьмая лабораторная работа</a></li>
                     <li><a href="/lab8/">Восьмая лабораторная работа</a></li>
+                    <li><a href="/lab9/">Девятая лабораторная работа</a></li>
                 </ol>
             </main>
             <footer>
