7132847cbdba24835e42065380cf45650aa92d02
Половко Милана <milana.polovko03@gmail.com>
2024-12-17 17:05:05 +0700

    Открытка

--- app.py
+++ app.py
@@ -48,7 +48,8 @@ if app.config['DB_TYPE']=='postgres':
     host_ip='127.0.0.1'
     host_port=5432
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = (f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}?client_encoding=utf8')
+    app.config['SQLALCHEMY_DATABASE_URI'] = \
+        f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}?client_encoding=utf8'
 else:
     dir_path=path.dirname(path.realpath(__file__))
     db_path=path.join(dir_path,"milana_polovko_orm.db")
